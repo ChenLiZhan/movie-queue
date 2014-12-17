4afbf3d938ae9143be81dd172327b4a0f41cc0cc
Soumya Ray <soumya.ray@gmail.com>
2014-12-17 16:06:09 +0800

    Removed logging code; Got demo_sns_pub working

--- app.rb
+++ app.rb
@@ -12,10 +12,15 @@ class NotificationSubscriber < Sinatra::Base
 
   helpers do
     def save_message(subject, message)
-      sns_note = Notification.new
-      sns_note.subject = subject
-      sns_note.message = message
-      return sns_note.save
+      begin
+        sns_note = Notification.new
+        sns_note.subject = subject
+        sns_note.message = message
+        sns_note.save
+      rescue => e
+        logger.error e
+        halt 500, "Failed to save message to data store"
+      end
     end
   end
 
@@ -27,27 +32,18 @@ class NotificationSubscriber < Sinatra::Base
     begin
       sns_msg_type = request.env["HTTP_X_AMZ_SNS_MESSAGE_TYPE"]
       sns_note = JSON.parse request.body.read
-      logger.info "SNS_MSG_TYPE: #{sns_msg_type}"
-      logger.info "SNS_MSG: #{sns_note}"
 
       case sns_msg_type
       when 'SubscriptionConfirmation'
         sns_confirm_url = sns_note['SubscribeURL']
-        logger.info "SNS_CONFIRM_URL: #{sns_confirm_url}"
         sns_confirmation = HTTParty.get sns_confirm_url
-        logger.info "SNS_CONFIRMATION: #{sns_confirmation}"
       when 'Notification'
-        note_subject = sns_note['Subject']
-        note_message = sns_note['Message']
-        logger.info "MSG_SAVED: Subject: #{note_subject}"
-        logger.info "MSG_SAVED: Message: #{note_message}"
-        unless save_message note_subject, note_message
-          halt 500, "Failed to save message"
-        end
+        save_message sns_note['Subject'], sns_note['Message']
       end
-    rescue Exception => e
-      logger.error "EXCEPTION: #{e.message}"
-      halt 400, "Could not process SNS notification"
+    rescue => e
+      logger.error e
+      halt 400, "Could not fully process SNS notification"
+      return
     end
 
     status 200
